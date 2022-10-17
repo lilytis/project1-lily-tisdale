c3003912a88e48d8282ec6ebbc55826e4ad91231
Lilyanne Tisdale <lilyanne.tisdale@gmail.com>
2022-10-16 23:57:16 -0500

    Update project1.py -- including all info from tmdb

--- project1.py
+++ project1.py
@@ -13,6 +13,7 @@ def get_movies():
     MOVIE_IDS = [10719, 50014, 222935]
     MOVIE_PATH = f'/movie/{MOVIE_IDS[randrange(3)]}'
     MOVIE_API_BASE_URL = f'https://api.themoviedb.org/3{MOVIE_PATH}'
+    #WIKI_IMAGE_URL = ''
 
     response = requests.get(
         MOVIE_API_BASE_URL,
@@ -20,20 +21,23 @@ def get_movies():
             'api_key': os.getenv('TMDB_API_KEY')
         }
     )
-    json_data = response.json()
 
-    this_movie = json_data
-    movie_data = this_movie['title']
+    this_movie = response.json()
+    genres_of_movie = ""
+    for genre in response['genres']:
+        genres_of_movie += str(genre['name']) + ", "
 
-    print(movie_data)
-
-get_movies()
+    movie_info = [this_movie['original_title'], this_movie['overview']]
+    
+    return movie_info
+    #print(movie_info)
 
 app = flask.Flask(__name__)
-app.secret_key = 'lmtlmtlmt'
+#app.secret_key = 'lmtlmtlmt'
 
 @app.route('/')
-def wesbite_title():
+def website_title():
+    get_movies();
     return flask.render_template('website.html')
 
-app.run(debug=True)
+app.run()
